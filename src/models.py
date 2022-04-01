471bc5222f163d9885d46e00c1f0201a4edd6ba4
Nicolas Liendro <nicolasliendro0697@gmail.com>
2022-04-01 04:12:59 -0300

    Add models for Activity

--- src/models.py
+++ src/models.py
@@ -1,7 +1,8 @@
 import requests
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from .services import SQLService
 
 sql_service = SQLService()
@@ -29,3 +30,20 @@ class Account(Base):
         res = requests.get(url)
         dolar_bolsa = float(res.json()[4]['casa']['venta'].replace(",", "."))
         return self.balance_available * dolar_bolsa
+
+
+class Activity(Base):
+    __tablename__ = 'activities'
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime(timezone=True), onupdate=func.now())
+    clients_id = Column(Integer, ForeignKey('clients.id'))
+    client = relationship(Client)
+
+
+class ActivityDetail(Base):
+    __tablename__ = 'activities_detail'
+    id = Column(Integer, primary_key=True)
+    activities_id = Column(Integer, ForeignKey('activities.id'))
+    activity = relationship("Activity")
+    type_of = Column(String)
+    amount = Column(Float)
