aa6f840128bc03bd65ab9def64119776e61f86a2
Nicolas Liendro <nicolasliendro0697@gmail.com>
2022-04-01 04:12:16 -0300

    Model for account with get_total_usd method

--- src/models.py
+++ src/models.py
@@ -1,5 +1,7 @@
+import requests
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
 from .services import SQLService
 
 sql_service = SQLService()
@@ -13,3 +15,17 @@ class Client(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String)
+
+
+class Account(Base):
+    __tablename__ = 'account'
+    id = Column(Integer, primary_key=True)
+    clients_id = Column(Integer, ForeignKey('clients.id'))
+    client = relationship("Client")
+    balance_available = Column(Float)
+
+    def get_total_usd(self):
+        url = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales'
+        res = requests.get(url)
+        dolar_bolsa = float(res.json()[4]['casa']['venta'].replace(",", "."))
+        return self.balance_available * dolar_bolsa
